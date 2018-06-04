2f03acd81fc73ef0beb67a1c26c0122f1b5f3c4d
Lynn Cyrin <lynncyrin@gmail.com>
2018-06-04 02:22:38 -0700

    get test script down to just the most relevant code

--- test/server_utils.py
+++ test/server_utils.py
@@ -7,29 +7,15 @@ import signal
 from config import PORT, SERVER_START_TIMEOUT, PROCFILE_PATH
 
 
-def start_server_process():
-    try:
-        # check for a currently existing server, and kill it if so
-        requests.get(f'http://localhost:{PORT}')
-        kill_server_processes()
-    except requests.exceptions.ConnectionError:
-        # start up our new server
-        subprocess.Popen(f'heroku local -p {PORT}', shell=True)
-
-
-def kill_server_processes():
-    with open(PROCFILE_PATH, 'r') as procfile:
-        server_start_command = procfile.read().split('web: ')[1].strip()
-    pids = [
-        process.split(' ')[0]
-        for process in subprocess.run('ps', stdout=subprocess.PIPE).stdout.decode('utf-8').split('\n')
-        if server_start_command in process
-    ]
-    for pid in pids:
+def with_active_server(func):
+    def wrapper():
+        _start_server_process()
         try:
-            os.kill(int(pid), signal.SIGTERM)
-        except ProcessLookupError:
+            func()
+        except Exception:
             pass
+        _kill_server_processes()
+    return wrapper
 
 
 def await_server_response(path):
@@ -42,3 +28,28 @@ def await_server_response(path):
     else:
         raise Exception(
             f'Could not connect to server within {SERVER_START_TIMEOUT} seconds, try re-running the tests?')
+
+
+def _start_server_process():
+    try:
+        # check for a currently existing server, and kill it if so
+        requests.get(f'http://localhost:{PORT}')
+        _kill_server_processes()
+    except requests.exceptions.ConnectionError:
+        # start up our new server
+        subprocess.Popen(f'heroku local -p {PORT}', shell=True)
+
+
+def _kill_server_processes():
+    with open(PROCFILE_PATH, 'r') as procfile:
+        server_start_command = procfile.read().split('web: ')[1].strip()
+    pids = [
+        process.split(' ')[0]
+        for process in subprocess.run('ps', stdout=subprocess.PIPE).stdout.decode('utf-8').split('\n')
+        if server_start_command in process
+    ]
+    for pid in pids:
+        try:
+            os.kill(int(pid), signal.SIGTERM)
+        except ProcessLookupError:
+            pass
--- test/test_server_init.py
+++ test/test_server_init.py
@@ -1,16 +1,8 @@
 import server_utils
 
 
-SERVER_TEST_PATH = '/ping'
-SERVER_TEST_RESULT = 'pong'
-
-
-def test_server_init():
-    server_utils.start_server_process()
-    try:
-        response = server_utils.await_server_response(SERVER_TEST_PATH)
-        assert response.status_code == 200
-        assert response.content.decode('utf-8') == SERVER_TEST_RESULT
-    except Exception:
-        pass
-    server_utils.kill_server_processes()
+@server_utils.with_active_server
+def test_ping_pong():
+    response = server_utils.await_server_response('/ping')
+    assert response.status_code == 200
+    assert response.content.decode('utf-8') == 'pong'
