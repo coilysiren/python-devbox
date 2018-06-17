8d26d850e4fefabdf288bc71c60888b15a737ab1
[[ BOT ]] Lynn Cyrin <lynncyrin@gmail.com>
2018-06-17 11:51:42 -0700

    tests for our basic routes

--- src/python/test.py
+++ src/python/test.py
@@ -1,7 +1,19 @@
 from . import app
+import json
+import sys
+
+
+def json_body(response):
+    return json.loads(response.get_data().decode(sys.getdefaultencoding()))
 
 
 def test_index():
     test_app = app.test_client()
     response = test_app.get('/')
-    assert response.body == 'hello world!!'
+    assert response.get_data(as_text=True) == 'hello world!!'
+
+
+def test_ping():
+    test_app = app.test_client()
+    response = test_app.get('/api/ping')
+    assert json_body(response) == {'data': 'pong'}
