639a44058c9fcbe483570ffb47f92ef43d5c9017
[[ BOT ]] Lynn Cyrin <lynncyrin@gmail.com>
2018-06-17 13:56:38 -0700

    cleanup, text input spec

--- src/python/test_snippets_post.py
+++ src/python/test_snippets_post.py
@@ -5,7 +5,6 @@ from .test_helper import json_body, app, db, session
 
 
 def test_post_snipped_requires_data(app, session):
-    # TODO, sync with product manager RE empty snippets
     # function under test
     response = app.post(
         '/snippets',
@@ -15,7 +14,6 @@ def test_post_snipped_requires_data(app, session):
 
 
 def test_post_snipped_requires_data_text(app, session):
-    # TODO, sync with product manager RE empty snippets
     # function under test
     response = app.post(
         '/snippets',
@@ -26,7 +24,6 @@ def test_post_snipped_requires_data_text(app, session):
 
 
 def test_post_snipped_requires_json(app, session):
-    # TODO, sync with product manager RE empty snippets
     # function under test
     response = app.post(
         '/snippets',
@@ -37,7 +34,6 @@ def test_post_snipped_requires_json(app, session):
 
 
 def test_post_snipped_requires_content_type(app, session):
-    # TODO, sync with product manager RE empty snippets
     # function under test
     response = app.post(
         '/snippets',
@@ -48,7 +44,6 @@ def test_post_snipped_requires_content_type(app, session):
 
 
 def test_post_returns_snippet(app, session):
-    # TODO, sync with product manager RE empty snippets
     # function under test
     response = app.post(
         '/snippets',
@@ -61,7 +56,6 @@ def test_post_returns_snippet(app, session):
 
 
 def test_post_respects_text_input(app, session):
-    # TODO, sync with product manager RE empty snippets
     # function under test
     response = app.post(
         '/snippets',
@@ -71,6 +65,18 @@ def test_post_respects_text_input(app, session):
     # assertion
     assert json_body(response)['text'] == 'rawr'
     assert response.status_code == 201
+
+
+def test_post_requires_text_input(app, session):
+    # TODO, sync with product manager RE empty snippets
+    # function under test
+    response = app.post(
+        '/snippets',
+        data=json.dumps({'text': ''}),
+        content_type='application/json',
+    )
+    # assertion
+    assert response.status_code == 400
 
 
 def test_post_respects_share_input_false(app, session):
