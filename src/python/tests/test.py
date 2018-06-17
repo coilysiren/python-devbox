18729d8bd7befce015e2609347dea0a869d77d25
[[ BOT ]] Lynn Cyrin <lynncyrin@gmail.com>
2018-06-17 16:55:34 -0700

    update test fixture

--- src/python/tests/test.py
+++ src/python/tests/test.py
@@ -3,14 +3,14 @@ import sys
 
 from ..server import SnippetModel
 from .test_helper import json_body
-from .test_fixtures import app, db, session
+from .test_fixtures import app, db, session, test_app
 
 
-def test_index(app):
+def test_index(test_app):
     response = app.get('/')
     assert response.get_data(as_text=True) == 'hello world!!'
 
 
-def test_ping(app):
+def test_ping(test_app):
     response = app.get('/api/ping')
     assert json_body(response) == {'data': 'pong'}
