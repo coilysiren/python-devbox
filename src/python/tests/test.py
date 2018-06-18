5f7e0f41990f8ac3468f0aa3d0afd42c60a08c1f
[[ BOT ]] Lynn Cyrin <lynncyrin@gmail.com>
2018-06-17 17:05:49 -0700

    update fixture name (again)

--- src/python/tests/test.py
+++ src/python/tests/test.py
@@ -7,10 +7,10 @@ from .test_fixtures import app, db, session, test_app
 
 
 def test_index(test_app):
-    response = app.get('/')
+    response = test_app.get('/')
     assert response.get_data(as_text=True) == 'hello world!!'
 
 
 def test_ping(test_app):
-    response = app.get('/api/ping')
+    response = test_app.get('/api/ping')
     assert json_body(response) == {'data': 'pong'}
