3aea05438e21700ab494ef33adf7a46950262bae
[[ BOT ]] Lynn Cyrin <lynncyrin@gmail.com>
2018-06-17 19:50:42 -0700

    remove standalone test folder

--- test/test_server_init.py
+++
@@ -1,8 +0,0 @@
-import server_utils
-
-
-@server_utils.with_active_server
-def test_ping_pong():
-    response = server_utils.await_server_response('/ping')
-    assert response.status_code == 200
-    assert response.content.decode('utf-8') == 'pong'
