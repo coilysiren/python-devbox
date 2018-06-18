c2894c680b89725bd7bc3d4f2caa209b01bca951
[[ BOT ]] Lynn Cyrin <lynncyrin@gmail.com>
2018-06-18 02:11:43 -0700

    server setup infra

--- makefile
+++ makefile
@@ -6,3 +6,7 @@ focus:
 
 run:
 	FLASK_ENV=development FLASK_APP=src/python/server flask run --port 3000
+
+reset-db:
+	python -c 'from src import db, app; db.drop_all(app=app)'
+	python -c 'from src import db, app; db.create_all(app=app)'
--- src/python/server.py
+++ src/python/server.py
@@ -20,7 +20,8 @@ from .models import db, SnippetModel, LikeModel, ShareModel
 load_dotenv(find_dotenv())
 app = Flask(__name__)
 app.secret_key = os.environ['SECRET_KEY']
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/pythondevbox_v4.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/pythondevbox_v5.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
 db.init_app(app)
 
