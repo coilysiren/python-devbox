a25c7618aaabfc3db83abbc700f0d88b4dca114e
[[ BOT ]] Lynn Cyrin <lynncyrin@gmail.com>
2018-06-18 01:46:41 -0700

    more achievement specs

--- src/python/tests/test_achievements.py
+++ src/python/tests/test_achievements.py
@@ -102,3 +102,113 @@ def test_get_achievements_shared(test_app, session):
     assert response.status_code == 200
     assert body
     assert int(body['shared']) == snips / 10
+
+
+def test_get_achievements_like_and_unlike_still_grants_achievements(test_app, session):
+    # setup
+    snips = 20
+    responses = [
+        test_app.post(
+            '/snippets',
+            json={'text': 'example', 'shared': True},
+            headers={'Authorization': 'lynncyrin@gmail.com'}
+        )
+        for i in range(snips)
+    ]
+    for response in responses:
+        snippet_id = json_body(response)['id']
+        test_app.put(
+            f'/snippets/{snippet_id}',
+            json={'liked': True},
+            headers={'Authorization': 'totallynotlynncyrin@gmail.com'}
+        )
+        test_app.put(
+            f'/snippets/{snippet_id}',
+            json={'liked': False},
+            headers={'Authorization': 'totallynotlynncyrin@gmail.com'}
+        )
+    # function under test
+    response = test_app.get(
+        '/achievements',
+        headers={'Authorization': 'lynncyrin@gmail.com'}
+    )
+    # assertion
+    body = json_body(response)
+    assert response.status_code == 200
+    assert body
+    assert int(body['liked']) == snips / 10
+
+
+def test_get_achievements_share_and_unshare_still_grants_achievements(test_app, session):
+    # setup
+    snips = 20
+    responses = [
+        test_app.post(
+            '/snippets',
+            json={'text': 'example', 'shared': True},
+            headers={'Authorization': 'lynncyrin@gmail.com'}
+        )
+        for i in range(snips)
+    ]
+    for response in responses:
+        snippet_id = json_body(response)['id']
+        test_app.put(
+            f'/snippets/{snippet_id}',
+            json={'shared': True},
+            headers={'Authorization': 'totallynotlynncyrin@gmail.com'}
+        )
+        test_app.put(
+            f'/snippets/{snippet_id}',
+            json={'shared': True},
+            headers={'Authorization': 'totallynotlynncyrin@gmail.com'}
+        )
+    # function under test
+    response = test_app.get(
+        '/achievements',
+        headers={'Authorization': 'totallynotlynncyrin@gmail.com'}
+    )
+    # assertion
+    body = json_body(response)
+    assert response.status_code == 200
+    assert body
+    assert int(body['shared']) == snips / 10
+
+
+def test_get_achievements_share_and_unshare_reshare_grants_double_achievements(test_app, session):
+    # setup
+    snips = 20
+    responses = [
+        test_app.post(
+            '/snippets',
+            json={'text': 'example', 'shared': True},
+            headers={'Authorization': 'lynncyrin@gmail.com'}
+        )
+        for i in range(snips)
+    ]
+    for response in responses:
+        snippet_id = json_body(response)['id']
+        test_app.put(
+            f'/snippets/{snippet_id}',
+            json={'shared': True},
+            headers={'Authorization': 'totallynotlynncyrin@gmail.com'}
+        )
+        test_app.put(
+            f'/snippets/{snippet_id}',
+            json={'shared': False},
+            headers={'Authorization': 'totallynotlynncyrin@gmail.com'}
+        )
+        test_app.put(
+            f'/snippets/{snippet_id}',
+            json={'shared': True},
+            headers={'Authorization': 'totallynotlynncyrin@gmail.com'}
+        )
+    # function under test
+    response = test_app.get(
+        '/achievements',
+        headers={'Authorization': 'totallynotlynncyrin@gmail.com'}
+    )
+    # assertion
+    body = json_body(response)
+    assert response.status_code == 200
+    assert body
+    assert int(body['shared']) == 2 * snips / 10
